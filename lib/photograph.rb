2ebe1783ab18fa5f094f785ca4465bd0a75eae52
Graham Thompson <grwthomps@gmail.com>
2019-09-17 09:16:00 -0600

    Create initialize method

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,3 +1,12 @@
 class Photograph
 
+  attr_reader :id, :name, :artist_id, :year
+
+  def initialize(attribute_hash)
+    @id = attribute_hash[:id]
+    @name = attribute_hash[:name]
+    @artist_id = attribute_hash[:artist_id]
+    @year = attribute_hash[:year]
+  end
+
 end
