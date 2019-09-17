9c495f37486ab157b250c4dccee35eddea1862e1
Graham Thompson <grwthomps@gmail.com>
2019-09-17 09:35:24 -0600

    Add test for adding artists in Curator class, expand initialization test

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -20,6 +20,20 @@ class CuratorTest < Minitest::Test
          artist_id: "2",
          year: "1941"
     })
+    @artist_1 = Artist.new({
+        id: "1",
+        name: "Henri Cartier-Bresson",
+        born: "1908",
+        died: "2004",
+        country: "France"
+    })
+    @artist_2 = Artist.new({
+        id: "2",
+        name: "Ansel Adams",
+        born: "1902",
+        died: "1984",
+        country: "United States"
+    })
   end
 
   def test_it_exists
@@ -28,6 +42,7 @@ class CuratorTest < Minitest::Test
 
   def test_initialization
     assert_equal [], @curator.photographs
+    assert_equal [], @curator.artists
   end
 
   def test_adding_photos
@@ -35,6 +50,13 @@ class CuratorTest < Minitest::Test
     assert_equal [@photo_1], @curator.photographs
     @curator.add_photograph(@photo_2)
     assert_equal [@photo_1, @photo_2], @curator.photographs
+  end
+
+  def test_adding_artists
+    @curator.add_artist(@artist_1)
+    assert_equal [@artist_1], @curator.artists
+    @curator.add_artist(@artist_2)
+    assert_equal [@artist_1, @artist_2], @curator.artists
   end
 
 end
