0d7319b20da018a1737a44e8cf2701af430be33b
Graham Thompson <grwthomps@gmail.com>
2019-09-17 09:22:46 -0600

    Create initialize method in Artist class

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,13 @@
 class Artist
 
+  attr_reader :id, :name, :born, :died, :country
+
+  def initialize(attribute_hash)
+    @id = attribute_hash[:id]
+    @name = attribute_hash[:name]
+    @born = attribute_hash[:born]
+    @died = attribute_hash[:died]
+    @country = attribute_hash[:country]
+  end
+
 end
