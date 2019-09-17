26fd19df1eb9235feecb36841912d410bc5fbf87
Graham Thompson <grwthomps@gmail.com>
2019-09-17 09:27:21 -0600

    Add Curator class tests for existence and initialization

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -4,4 +4,16 @@ require './lib/curator'
 
 class CuratorTest < Minitest::Test
 
+  def setup
+    @curator = Curator.new
+  end
+
+  def test_it_exists
+    assert_instance_of Curator, @curator
+  end
+
+  def test_initialization
+    assert_equal [], @curator.photographs
+  end
+
 end
